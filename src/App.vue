46d5236cd26a026295496ca4edb390974260ee7a
maty <matikster1@wp.pl>
2022-06-11 12:52:10 +0200

    Create an empty tip Box Component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="container">
-    <boardGameComponent />
-    <playAgainComponent />
+    <board-game-component></board-game-component>
+    <play-again-component></play-again-component>
+    <tip-box-component v-show="tipAvailable"></tip-box-component>
   </div>
 </template>
 
 <script>
 import "@/styles/globalStyles.css";
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+import { useMainStore } from "@/stores/MainStore";
 import boardGameComponent from "./components/boardGameComponent.vue";
 import playAgainComponent from "./components/playAgainComponent.vue";
+import tipBoxComponent from "./components/tip/tipBoxComponent.vue";
 
 export default {
   name: "App",
   components: {
     boardGameComponent,
     playAgainComponent,
+    tipBoxComponent,
+  },
+  setup() {
+    const { getFinishGame } = storeToRefs(useMainStore());
+
+    const tipAvailable = computed(() => !getFinishGame.value);
+
+    return { tipAvailable };
   },
 };
 </script>
---
+++ src/components/tip/tipBoxComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="icon noselect">?</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tipBoxComponent",
+};
+</script>
+
+<style scoped>
+.icon {
+  display: flex;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #42b983;
+  margin: 30px auto;
+  color: #ffffff;
+  font-size: 30px;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  cursor: pointer;
+}
+</style>
